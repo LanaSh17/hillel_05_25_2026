92c9252645404420ea38c8ea12b74e02b77b2127
lana <marilan171191@gmail.com>
2026-06-01 16:43:12 +0400

    added something

---
+++ lesson1/lesson1.py
@@ -0,0 +1,182 @@
+from turtledemo.tree import tree
+
+print("Hello", end = " ")
+print("world!")
+
+print('Hello world!')
+
+my_int = 10
+# another_int = 20
+# another_int = my_int
+# print(id(my_int))
+# print(id(another_int))
+
+
+my_float = 15.3
+#print(type(my_float))
+
+# my_sum = int(my_int + my_float)
+my_sum = my_int + my_float
+my_sum = int(my_sum)
+print(my_sum)
+
+
+# my_string = "Hello"
+# my_list = [1, 2, 5.9, True, [1, 0],]
+# my_tuple = (1, 2, None)
+# my_set = {1, 2, False}
+# my_dict = {"key" : "value", "key2" : {"sub_key" : "sub_value"}}
+#
+# my_bool = True
+# my_none = None
+#
+# my_variable = None
+# print(my_variable)
+# my_variable = my_int + my_float
+#print(my_variable)
+
+# ctrl + /
+
+# snake_case - наименования перемнных
+# CamelCase - наименования классов
+# UPPER_SNAKE - с большой буквы, _ между словами
+
+MY_VARIABLE = 1
+print(MY_VARIABLE)
+MY_VARIABLE = "Hello"
+print(MY_VARIABLE)
+
+my_var = 10
+
+_ = "Trash variable"
+
+# BASE_PAGE_URL = "google.com"
+# # ctrl + alt + L - форматирует код, ставит нужные пробелы
+# sum_ = 10 + 10
+# diff = 15 - 10
+# mult = 5 * 12
+# div = 50 / 5
+#
+# sum({1, 2, 3})
+#
+# print(sum_)
+# print(diff)
+# print(mult)
+# print(div)
+
+# = - оператор присвоения
+# == - оператор сравнения, взвращает True\False
+
+'''
+if sum_ == 15:
+   print("sum is equal to 15")
+   if div == 0:
+       print("div is equal to 0")
+'''
+my_name, my_age = "Alex", 30
+# print("Hello", my_name, end="")
+# print("Hello", my_name)
+
+
+copy_age, cope_name = my_age, my_name
+
+print("Hello my name is", my_name, "My age is", my_age)
+print(cope_name, copy_age)
+
+hello = "Hello"
+world = "world"
+
+# if True:
+#     print(f"{hello} {world}!")
+#
+for letter in "Hello world!":
+    print(letter)
+
+# apples = 2
+# banana = apples * 4
+# print(banana)
+   for letter in "Hello world!":
+   
+#      print(letter)
+# storona_one= 1
+# storona_two = 2
+# storona_three = 3
+# storona_four = 4
+#
+# perimetery = storona_one + storona_two + storona_three + storona_four
+# print(perimetery)
+
+
+
+# apple_tree = 4
+# pear_tree = apple_tree + 5
+# plum_tree = apple_tree - 2
+#
+# my_variable = (apple_tree, pear_tree, plum_tree)
+#
+# print(my_variable)
+#
+#
+# temperature = 5 + 0
+# afternoon_temperature = temperature - 10
+# evening_temperature = afternoon_temperature + 4
+#
+# print(evening_temperature
+#
+#
+
+# boys = 24
+# girls = 24 / 2
+# today_boy = boys - 1
+# today_girl = girls - 2
+#
+# my_variable = (today_boy, today_girl)
+#
+# print(sum(my_variable))
+
+# first_book_cost = 8
+# second_book_cost = first_book_cost + 2
+# third_book_cost = (first_book_cost  + second_book_cost) / 2
+#
+# all_books = (first_book_cost, second_book_cost, third_book_cost)
+#
+# print(sum(all_books))
+# all_boys_in_theater = 24
+# all_girls_in_theater = 24 / 2
+# today_one_boy_fell_ill = all_boys_in_theater - 1
+# today_two_girls_not_come = all_girls_in_theater - 2
+#
+# all_children_today = (today_one_boy_fell_ill, today_two_girls_not_come)
+# print(sum(all_children_today))
+# planted_apple = 4
+# planted_pear = planted_apple + 5
+# planted_plum = planted_apple - 2
+#
+# all_trees = (planted_apple, planted_pear, planted_plum)
+#
+# all_trees_cost = sum(all_trees)
+# print(all_trees_cost)
+
+first_book_cost = 8
+second_book_cost = first_book_cost + 2
+third_book_cost = (first_book_cost  + second_book_cost) / 2
+
+all_books = (first_book_cost, second_book_cost, third_book_cost)
+
+all_books_cost = sum(all_books)
+
+print(all_books_cost)
+
+storona_1 = 1
+storona_2 = 2
+сторона_3 = 3
+storona_4 = 4
+
+perimetery = storona_1 + storona_2 + сторона_3 + storona_4
+print(perimetery)
+# task 01 == Виправте синтаксичні помилки
+
+
+
+
+
---
+++ python_practice/lesson2/second_file.py
@@ -0,0 +1,5 @@
+if True
+    print("It was True")
+
+my_variable = 15
+
